f794370c2fedde4e9e29668e22fa6a7c34c040bf
kasumalgarh <gajrajsinghrathore8@gmail.com>
2025-12-13 21:48:02 +0530

    renew website 13.12.2025
    renew website 13.12.2025

--- test/8979155-a13e68283cd1bb39362e0df0e17f32e0027c72f2/fadeout.js
+++
@@ -1,9 +0,0 @@
-//#1 http://stackoverflow.com/questions/15118802/jquery-fade-out-div-display-after-certain-set-time
-
-$("#plz").fadeOut('slow').delay(5000).fadeOut('slow');
-
-//or http://stackoverflow.com/questions/17677398/hide-a-div-after-5-seconds-or-when-the-user-clicks-the-close-link
-
-$(window).load(function(){
-  setTimeout(function(){ $('.video-field-new').fadeOut() }, 5000);
-});
--- test/foundationcdn-master/README.md
+++
@@ -1,6 +0,0 @@
-# FoundationCDN
-
-All code merged to `master` is auto-deployed to the live site.
-The site is completely static.
-
-Feel free to send PRs to fix bugs or add new features!
